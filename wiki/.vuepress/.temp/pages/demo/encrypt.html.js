b68d7264b9fa0eb4098f21091915493bd2f4c878
super <super@example.com>
2025-10-05 11:54:00 +0800

    auto update

--- wiki/.vuepress/.temp/pages/demo/encrypt.html.js
+++ wiki/.vuepress/.temp/pages/demo/encrypt.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/demo/encrypt.html.vue"
-const data = JSON.parse("{\"path\":\"/demo/encrypt.html\",\"title\":\"密码加密的文章\",\"lang\":\"zh-CN\",\"frontmatter\":{\"icon\":\"lock\",\"category\":[\"使用指南\"],\"tag\":[\"加密\"],\"feed\":false,\"seo\":false,\"head\":[[\"link\",{\"rel\":\"alternate\",\"hreflang\":\"en-us\",\"href\":\"https://zenithworld.com/en/demo/encrypt.html\"}]]},\"git\":{},\"readingTime\":{\"minutes\":0.51,\"words\":154},\"filePathRelative\":\"demo/encrypt.md\"}")
+const data = JSON.parse("{\"path\":\"/demo/encrypt.html\",\"title\":\"密码加密的文章\",\"lang\":\"zh-CN\",\"frontmatter\":{\"icon\":\"lock\",\"category\":[\"使用指南\"],\"tag\":[\"加密\"],\"feed\":false,\"seo\":false,\"head\":[[\"link\",{\"rel\":\"alternate\",\"hreflang\":\"en-us\",\"href\":\"https://zenithworld.com/en/demo/encrypt.html\"}]]},\"git\":{\"createdTime\":1759636241000,\"updatedTime\":1759636241000},\"readingTime\":{\"minutes\":0.51,\"words\":154},\"filePathRelative\":\"demo/encrypt.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
