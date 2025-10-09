24ad981336eb1fee1fe9fbf9806d9a271165edcc
super <super@example.com>
2025-10-09 16:50:18 +0800

    enable github pages

--- wiki/.vuepress/.temp/internal/routes.js
+++ wiki/.vuepress/.temp/internal/routes.js
@@ -6,6 +6,7 @@ export const routes = Object.fromEntries([
   ["/demo-0.0.1/", { loader: () => import(/* webpackChunkName: "demo-0.0.1_index.html" */"C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/demo-0.0.1/index.html.js"), meta: {"title":"世界观总览","icon":"laptop-code"} }],
   ["/demo-0.0.1/timeline.html", { loader: () => import(/* webpackChunkName: "demo-0.0.1_timeline.html" */"C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/demo-0.0.1/timeline.html.js"), meta: {"title":"世界大事年表"} }],
   ["/demo-0.0.1/character/", { loader: () => import(/* webpackChunkName: "demo-0.0.1_character_index.html" */"C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/demo-0.0.1/character/index.html.js"), meta: {"title":"角色介绍","icon":"user"} }],
+  ["/demo-0.0.1/character/CCO/", { loader: () => import(/* webpackChunkName: "demo-0.0.1_character_CCO_index.html" */"C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/demo-0.0.1/character/CCO/index.html.js"), meta: {"title":""} }],
   ["/demo-0.0.1/character/divineproxy/", { loader: () => import(/* webpackChunkName: "demo-0.0.1_character_divineproxy_index.html" */"C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/demo-0.0.1/character/divineproxy/index.html.js"), meta: {"title":""} }],
   ["/demo-0.0.1/character/superhero/111.html", { loader: () => import(/* webpackChunkName: "demo-0.0.1_character_superhero_111.html" */"C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/demo-0.0.1/character/superhero/111.html.js"), meta: {"title":""} }],
   ["/demo-0.0.1/character/superhero/character-EtherealKnight.html", { loader: () => import(/* webpackChunkName: "demo-0.0.1_character_superhero_character-EtherealKnight.html" */"C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/demo-0.0.1/character/superhero/character-EtherealKnight.html.js"), meta: {"title":"灵动骑士"} }],
@@ -16,7 +17,6 @@ export const routes = Object.fromEntries([
   ["/demo-0.0.1/character/superhero/character-StrikeClock.html", { loader: () => import(/* webpackChunkName: "demo-0.0.1_character_superhero_character-StrikeClock.html" */"C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/demo-0.0.1/character/superhero/character-StrikeClock.html.js"), meta: {"title":""} }],
   ["/demo-0.0.1/character/superhero/character-WizardHope.html", { loader: () => import(/* webpackChunkName: "demo-0.0.1_character_superhero_character-WizardHope.html" */"C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/demo-0.0.1/character/superhero/character-WizardHope.html.js"), meta: {"title":""} }],
   ["/demo-0.0.1/character/superhero/", { loader: () => import(/* webpackChunkName: "demo-0.0.1_character_superhero_index.html" */"C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/demo-0.0.1/character/superhero/index.html.js"), meta: {"title":"超级英雄"} }],
-  ["/demo-0.0.1/character/CCO/", { loader: () => import(/* webpackChunkName: "demo-0.0.1_character_CCO_index.html" */"C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/demo-0.0.1/character/CCO/index.html.js"), meta: {"title":""} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
 
