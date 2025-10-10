09e6cabfff6b71274cd451ab5e57406d4ff618c2
super <super@example.com>
2025-10-10 20:53:07 +0800

    auto update

---
+++ wiki/.vuepress/.temp/pages/demo-0.0.1/character/superhero/1 copy.html.js
@@ -0,0 +1,16 @@
+import comp from "C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/demo-0.0.1/character/superhero/1 copy.html.vue"
+const data = JSON.parse("{\"path\":\"/demo-0.0.1/character/superhero/1%20copy.html\",\"title\":\"[1][WizardHope]\",\"lang\":\"zh-CN\",\"frontmatter\":{\"sidebar\":false,\"description\":\"[1][WizardHope] 基本信息 姓名：\"},\"git\":{},\"readingTime\":{\"minutes\":0.03,\"words\":10},\"filePathRelative\":\"demo-0.0.1/character/superhero/1 copy.md\",\"autoDesc\":true}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
