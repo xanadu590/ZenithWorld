ae9e58fbc1d7c7dd1e25928a1851218c4fa888bb
super <super@example.com>
2025-10-10 21:47:21 +0800

    auto update

---
+++ wiki/.vuepress/.temp/pages/demo-0.0.1/character/superhero/6 copy.html.vue
@@ -0,0 +1,8 @@
+<template><div><h1 id="_6-wizardhope" tabindex="-1"><a class="header-anchor" href="#_6-wizardhope"><span>[6][WizardHope]</span></a></h1>
+<h2 id="基本信息" tabindex="-1"><a class="header-anchor" href="#基本信息"><span>基本信息</span></a></h2>
+<ul>
+<li>姓名：</li>
+</ul>
+</div></template>
+
+
---
+++ wiki/.vuepress/.temp/pages/demo-0.0.1/character/superhero/7.html.js
@@ -0,0 +1,16 @@
+import comp from "C:/Users/super/ZenithWorld/wiki/.vuepress/.temp/pages/demo-0.0.1/character/superhero/7.html.vue"
+const data = JSON.parse("{\"path\":\"/demo-0.0.1/character/superhero/7.html\",\"title\":\"[6][WizardHope]\",\"lang\":\"zh-CN\",\"frontmatter\":{\"sidebar\":false,\"description\":\"[6][WizardHope] 基本信息 姓名：\"},\"git\":{},\"readingTime\":{\"minutes\":0.03,\"words\":10},\"filePathRelative\":\"demo-0.0.1/character/superhero/7.md\",\"autoDesc\":true}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
