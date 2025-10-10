c5cfb7a579c2f4f6e492947fb58fc08e6144928f
super <super@example.com>
2025-10-10 20:58:04 +0800

    auto update

--- wiki/.vuepress/.temp/pages/demo-0.0.1/character/superhero/1 copy.html.vue
+++
@@ -1,8 +0,0 @@
-<template><div><h1 id="_1-wizardhope" tabindex="-1"><a class="header-anchor" href="#_1-wizardhope"><span>[1][WizardHope]</span></a></h1>
-<h2 id="基本信息" tabindex="-1"><a class="header-anchor" href="#基本信息"><span>基本信息</span></a></h2>
-<ul>
-<li>姓名：</li>
-</ul>
-</div></template>
-
-
