26895f0de6e852e245014844954cedf103217b2b
super <super@example.com>
2025-10-12 00:21:47 +0800

    Auto update

--- wiki/.vuepress/config.ts
+++ wiki/.vuepress/config.ts
@@ -7,7 +7,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 const isProd = process.env.NODE_ENV === 'production'
 
 export default defineUserConfig({
-  base: '/ZenithWorld/',
+  base:  isProd ? '/ZenithWorld/' : '/',
 
   locales: {
     "/en/": {
