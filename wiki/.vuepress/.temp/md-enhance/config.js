c3b1454211bd81cdf005598978caedde53bbcaef
super <super@example.com>
2025-10-13 21:52:57 +0800

    auto update

--- wiki/.vuepress/.temp/md-enhance/config.js
+++
@@ -1,9 +0,0 @@
-import CodeDemo from "C:/Users/super/ZenithWorld/node_modules/.pnpm/vuepress-plugin-md-enhance@_da4e39f0bf7c5bacfd5f21799242946a/node_modules/vuepress-plugin-md-enhance/lib/client/components/CodeDemo.js";
-import MdDemo from "C:/Users/super/ZenithWorld/node_modules/.pnpm/vuepress-plugin-md-enhance@_da4e39f0bf7c5bacfd5f21799242946a/node_modules/vuepress-plugin-md-enhance/lib/client/components/MdDemo.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("CodeDemo", CodeDemo);
-    app.component("MdDemo", MdDemo);
-  },
-};
--- wiki/.vuepress/.temp/pages/demo-0.0.1/character/superhero/character-StrikeClock.html.vue
+++
@@ -1,8 +0,0 @@
-<template><div><h1 id="警钟-strikeclock" tabindex="-1"><a class="header-anchor" href="#警钟-strikeclock"><span>[警钟][StrikeClock]</span></a></h1>
-<h2 id="基本信息" tabindex="-1"><a class="header-anchor" href="#基本信息"><span>基本信息</span></a></h2>
-<ul>
-<li>姓名：钟立明</li>
-</ul>
-</div></template>
-
-
