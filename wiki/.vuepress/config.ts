a13e2cd73f6d0c4e6860d7b80892b20c7f3f95a4
xanadu590 <143111913+xanadu590@users.noreply.github.com>
2025-11-18 12:26:52 +0800

    Update config.ts

--- wiki/.vuepress/config.ts
+++ wiki/.vuepress/config.ts
@@ -7,7 +7,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 const isProd = process.env.NODE_ENV === 'production'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/ZenithWorld/',
 
   locales: {
     "/en/": {
