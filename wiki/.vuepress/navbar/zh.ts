e97557d3ab5c2927f449f6fa194de8c73561ba45
super <super@example.com>
2025-12-01 16:42:05 +0800

    auto update

--- wiki/.vuepress/navbar/zh.ts
+++ wiki/.vuepress/navbar/zh.ts
@@ -4,6 +4,12 @@ export const zhNavbar = navbar([
   "/",
 
   {
+    text: '推荐文章',
+    icon: 'fire',
+    link: '/docs/hotpages.html',
+  },
+
+  {
     text: '高级搜索',
     icon: 'search',
     link: '/docs/advanced-search.html',
