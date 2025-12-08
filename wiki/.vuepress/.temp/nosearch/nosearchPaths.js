503f4459a16f6b5999787fc6d6fbaf94d9f7623c
super <super@example.com>
2025-12-08 15:45:26 +0800

    Auto update

--- wiki/.vuepress/.temp/nosearch/nosearchPaths.js
+++ wiki/.vuepress/.temp/nosearch/nosearchPaths.js
@@ -4,15 +4,15 @@ export const nosearchPaths = [
   "/docs/hotpages.html",
   "/docs/",
   "/docs/world/characters/",
-  "/docs/world/geography/",
   "/docs/world/factions/",
   "/docs/world/concepts/",
+  "/docs/world/geography/",
   "/docs/world/history/",
   "/docs/world/characters/CCO/",
   "/docs/world/characters/superhero/",
   "/docs/world/characters/divineproxy/",
-  "/docs/world/factions/heroic/",
   "/docs/world/factions/neutral/",
+  "/docs/world/factions/heroic/",
   "/docs/world/factions/official/",
   "/docs/world/factions/others/"
 ];
