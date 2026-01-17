2cd230a044dc13406ff24514ef04f8b9b3a546c5
super <super@example.com>
2026-01-17 12:13:55 +0800

    chore: remove vuepress temp/cache from git tracking

--- wiki/.vuepress/.temp/markdown-image/client.js
+++
@@ -1,3 +0,0 @@
-import "C:/Users/super/ZenithWorld/node_modules/.pnpm/@vuepress+plugin-markdown-i_d795f68058dab67eb2a3967162f1307f/node_modules/@vuepress/plugin-markdown-image/lib/client/styles/figure.css"
-import "C:/Users/super/ZenithWorld/node_modules/.pnpm/@vuepress+plugin-markdown-i_d795f68058dab67eb2a3967162f1307f/node_modules/@vuepress/plugin-markdown-image/lib/client/styles/mark.css"
-
--- wiki/.vuepress/.temp/pages/docs/advanced-search.html.vue
+++
@@ -1,4 +0,0 @@
-<template><div><p>这里是<span class="zw-auto-link-wrap"><RouterLink to="/" class="zw-auto-link zw-auto-link--first" data-tooltip="欢迎来到《巅峰世界》百科，英雄与反派的交织在这里上演。">巅峰世界</RouterLink><AutoLinkTooltip term="巅峰世界" to="/" tooltip="欢迎来到《巅峰世界》百科，英雄与反派的交织在这里上演。" first="true" /></span>百科的 <strong>高级搜索</strong>。你可以根据「人物 / 城市 / 组织 / <span class="zw-auto-link-wrap"><RouterLink to="/docs/world/concepts/" class="zw-auto-link zw-auto-link--first" data-tooltip="概念">概念</RouterLink><AutoLinkTooltip term="概念" to="/docs/world/concepts/" tooltip="概念" first="true" /></span>」等分类来筛选结果。</p>
-<MeiliFilterSearch /></div></template>
-
-
